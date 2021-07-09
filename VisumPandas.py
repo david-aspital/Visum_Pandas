2b89e71b87e73beb926b975e1c17c3f046ee9467
David Aspital <83019108+david-aspital@users.noreply.github.com>
2021-07-09 18:04:41 +0100

    Update VisumPandas.py

--- VisumPandas.py
+++ VisumPandas.py
@@ -1,40 +1,34 @@
 import pandas as pd
 import numpy as np
 import os
-import wx
-import datetime
-import time
-import traceback
 
 Visum = None
 PRIO = 20480
 
-def export_list(container, lla, path, name, filter=False):
+def export_list(container, layout, folderpath, filename, filter=False):
     # Create Visum list and export it to the given path
     visum_list = None
     try:
-        visum_list = eval(f'Visum.Lists.Create{}List',{'__builtins__':None},dispatcher)
+        visum_list = eval(f'Visum.Lists.Create{container}List',{'__builtins__':None})
     except:
         raise KeyError(f'"{container}" is not a valid Visum object.')
-    visum_list.OpenLayout(lla)
+    visum_list.OpenLayout(layout)
     if filter:
         visum_list.SetObjects(filter)
-    if name[-4:] == ".att":
-        pass
-    else:
-        name = name+".att"
-    visum_list.SaveToAttributeFile(f"{path}\\{name}",9)
+    if filename[-4:] != ".att":
+        filename = filename+".att"
+    visum_list.SaveToAttributeFile(os.path.join(folderpath, filename),9)
     visum_list.Close()
     visum_list = None
 
 
-def create_data_frame(list_type, layout, dir_name, temp_path, timeslice=None, filter=False):
+def create_data_frame(container, layout, folderpath, temp_path, timeslice=None, filter=False):
     # Create pandas df from Visum list (export as att to temp path and read back)
     visum_list = None
     bkslsh = "\\"
-    input_filename = layout.format(timeslice) if timeslice is not None else layout
-    input_filepath = os.path.join(dir_name, input_filename)
-    temp_file = f"{input_filename.replace('Inputs'+bkslsh,'')}.att"
-    export_list(list_type, input_filepath, temp_path, temp_file, filter)
-    data_frame = pd.read_csv(f"{temp_path}\\{temp_file}", sep="\t", header=2, skiprows=10)
+    filepath = os.path.join(folderpath, layout)
+    temp_file = f"{layout}.att"
+    export_list(container, filepath, temp_path, temp_file, filter)
+    data_frame = pd.read_csv(os.path.join(temp_path, temp_file), sep="\t", header=2, skiprows=10)
     return data_frame
+
