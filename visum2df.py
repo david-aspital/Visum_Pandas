7ed2e75f43678a92acbf315a2f8b68366cf00c1d
David Aspital <83019108+david-aspital@users.noreply.github.com>
2021-11-01 11:15:04 +0000

    Update to accommodate list objects

--- visum2df.py
+++ visum2df.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import tempfile
+import datetime
 
 
 def export_list(layout, filename=None, folderpath=None):
@@ -33,13 +34,15 @@ def export_list(layout, filename=None, folderpath=None):
     Visum.IO.SaveAttributeFile(os.path.join(folderpath, name), layout, 9)
 
 
-def create_data_frame(layout, temp_path=tempfile.gettempdir(), folderpath=None):
+def create_data_frame(visum_list=None, layout=None, temp_path=tempfile.gettempdir(), folderpath=None):
     '''Create pandas df from Visum list by exporting as .att to temp path and reading back as df
 
     Parameters
     ----------
-    layout : str
-        The layout file name (incuding extension). If no extension is provided, .llax will be assumed.
+    visum_list : obj, optional
+        The Visum list object (including all columns to be exported). Either layout or this variable needs to be provided.
+    layout : str, optional
+        The layout file name (incuding extension). If no extension is provided, .llax will be assumed. Either visum_list or this variable needs to be provided.
     temp_path : str, path object, optional
         The folder path of the temporary location to write attribute data to. By default this is the %TMP% location
     folderpath : str, path object, optional
@@ -50,17 +53,25 @@ def create_data_frame(layout, temp_path=tempfile.gettempdir(), folderpath=None):
     DataFrame
     '''
 
+    if (visum_list is None) and (layout is None):
+        raise ValueError("Either visum_list or layout need to be defined.")
+
     if folderpath is None:
         folderpath = os.path.dirname(Visum.IO.CurrentVersionFile)
 
-    name, ext = os.path.splitext(layout)
-    if not (ext == '.llax' or ext == '.lla'):
-        layout += '.llax'
-    filepath = os.path.join(folderpath, layout)
-    temp_file = f"{name}.att"
-    export_list(filepath, temp_file, temp_path)
+    if not (layout is None):
+        name, ext = os.path.splitext(layout)
+        if not (ext == '.llax' or ext == '.lla'):
+            layout += '.llax'
+        filepath = os.path.join(folderpath, layout)
+        temp_file = f"{name}.att"
+        export_list(filepath, temp_file, temp_path)
+    else:
+        temp_file = f'visum_list_{datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S")}.att'
+        visum_list.SaveToAttributeFile(os.path.join(temp_path, temp_file), 9)
     data_frame = pd.read_csv(os.path.join(temp_path, temp_file), sep="\t", header=2, skiprows=10)
     return data_frame
+
 
 if __name__ == '__main__':
     temp_path = tempfile.gettempdir()
